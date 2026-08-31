b50b057bba054c9678cf9068e6bc041f286400a4
Pierfrancescco <percepition@gmail.com>
2026-08-31 23:36:39 +0200

    Pulizia repository: rimossi file temporanei, build ed eseguibili obsoleti

--- Prova.py
+++
@@ -1,8 +0,0 @@
-
-
-def leggiVariabileDaFunzione():
-    somma = 5 + 2
-    
-leggiVariabileDaFunzione()
-
-print(leggiVariabileDaFunzione().somma)  # Questo genererà un errore poiché 'somma' non è definita nel contesto globale
--- provaCpp.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-
-    std::cin.get(); // Wait for user input before closing
-    return 0;
-}
